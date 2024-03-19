b359eb37476b0dbf57e9531d80d3efa1e1939f68
camtriggerhappy <60159291+camtriggerhappy@users.noreply.github.com>
2024-03-19 00:50:16 -0400

    fixed screen and buttons

--- InstrumentProgram.ino
+++ InstrumentProgram.ino
@@ -10,6 +10,7 @@
 #include <Adafruit_SSD1306.h>
 #include <splash.h>
 #include <Tone.h>
+#include <Wire.h>
 
 Tone t1;
 Tone t2;
@@ -18,62 +19,92 @@ Tone t4;
 Tone t5;
 Tone t6;
 Tone t7;
-LiquidCrystal_I2C lcd(serial, 128, 64)
+int currentOctave = 0;
+Adafruit_SSD1306 lcd(128, 64, &Wire, -1);
+
+const int bOctaves[7] = { NOTE_B1, NOTE_B2, NOTE_B3, NOTE_B4, NOTE_B5, NOTE_B6, NOTE_B7 };
+const int aOctaves[7] = { NOTE_A1, NOTE_A2, NOTE_A3, NOTE_A4, NOTE_A5, NOTE_A6, NOTE_A7 };
+const int gOctaves[7] = { NOTE_G1, NOTE_G2, NOTE_G3, NOTE_G4, NOTE_G5, NOTE_G6, NOTE_G7 };
+const int fOctaves[7] = { NOTE_F1, NOTE_F2, NOTE_F3, NOTE_F4, NOTE_F5, NOTE_F6, NOTE_F7 };
+const int eOctaves[7] = { NOTE_E1, NOTE_E2, NOTE_E3, NOTE_E4, NOTE_E5, NOTE_E6, NOTE_E7 };
+const int dOctaves[7] = { NOTE_D1, NOTE_D2, NOTE_D3, NOTE_D4, NOTE_D5, NOTE_D6, NOTE_D7 };
+int cOctaves[7] = { NOTE_C1, NOTE_C2, NOTE_C3, NOTE_C4, NOTE_C5, NOTE_C6, NOTE_C7 };
+
 
 void setup() {
   // put your setup code here, to run once:
+
+  if (!lcd.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+  }
+
+
+  delay(6);
   pinMode(2, INPUT);
-  t1.begin(2);
+
   pinMode(3, INPUT);
-  t2.begin(3);
+
   pinMode(4, INPUT);
-  t3.begin(4);
+
   pinMode(5, INPUT);
-  t4.begin(5);
+
   pinMode(6, INPUT);
-  t5.begin(6);
+
   pinMode(7, INPUT);
-  t6.begin(7);
+
   pinMode(8, INPUT);
-  t7.begin(8);
+  t1.begin(13);
   pinMode(13, OUTPUT);
-  Tone bOctaves[8] = [NOTE_B1, NOTE_B2, NOTE_B3, NOTE_B4, NOTE_B5, NOTE_B6, NOTE_B7, NOTE_B8];
-  Tone aOctaves[8] = [NOTE_A1, NOTE_A2, NOTE_A3, NOTE_A4, NOTE_A5, NOTE_A6, NOTE_A7, NOTE_A8];
-  Tone gOctaves[8] = [NOTE_G1, NOTE_G2, NOTE_G3, NOTE_G4, NOTE_G5, NOTE_G6, NOTE_G7, NOTE_G8];
-  Tone fOctaves[8] = [NOTE_F1, NOTE_F2, NOTE_F3, NOTE_F4, NOTE_F5, NOTE_F6, NOTE_F7, NOTE_F8];
-  Tone eOctaves[8] = [NOTE_E1, NOTE_E2, NOTE_E3, NOTE_E4, NOTE_E5, NOTE_E6, NOTE_E7, NOTE_E8];
-  Tone dOctaves[8] = [NOTE_D1, NOTE_D2, NOTE_D3, NOTE_D4, NOTE_D5, NOTE_D6, NOTE_D7, NOTE_D8];
-  Tone cOctaves[8] = [NOTE_C1, NOTE_C2, NOTE_C3, NOTE_C4, NOTE_C5, NOTE_C6, NOTE_C7, NOTE_C8];
-  int currentOctave = 0;
+
+  Serial.begin(115200);
 }
 
 void loop() {
+  lcd.clearDisplay();
   if (digitalRead(2)) {
-    play(bOctaves[currentOctave]);
-    lcd.print(bOctaves[currentOctave]);
+    t1.play(aOctaves[currentOctave]);
+
   } else if (digitalRead(3)) {
-    play(aOctaves[currentOctave]);
-    lcd.print(aOctaves[currentOctave]);
+    t1.play(bOctaves[currentOctave]);
+
   } else if (digitalRead(4)) {
-    play(gOctaves[currentOctave]);
-    lcd.print(gOctaves[currentOctave]);
+    t1.play(cOctaves[currentOctave]);
+
   } else if (digitalRead(5)) {
-    play(fOctaves[currentOctave]);
-    lcd.print(fOctaves[currentOctave]);
+    t1.play(dOctaves[currentOctave]);
+
   } else if (digitalRead(6)) {
-    play(eOctaves[currentOctave]);
-    lcd.print(eOctaves[currentOctave]);
+    t1.play(eOctaves[currentOctave]);
+
   } else if (digitalRead(7)) {
-    play(dOctaves[currentOctave]);
-    lcd.print(dOctaves[currentOctave]);
-  } else if (digitalRead(8)) [
-    play(cOctaves[currentOctave]);
-    lcd.print(cOctaves[currentOctave]);
-  ] else if (digitalRead(9)) {
-    currentOctave++;
-  } else if (digitalRead(10)) {
+    t1.play(fOctaves[currentOctave]);
+
+  } else if (digitalRead(8)) {
+
+  } else if (digitalRead(9)) {
+
     currentOctave--;
+    delay(300);
+
+  } else if (digitalRead(10)) {
+
+    currentOctave++;
+    delay(300);
+
   } else {
-    stop();
+    t1.stop();
   }
+  if (currentOctave > 6) {
+    currentOctave = 6;
+  }
+  if (currentOctave < 0) {
+    currentOctave = 0;
+  }
+
+  lcd.setTextSize(1);
+  lcd.setTextColor(WHITE);
+  lcd.setCursor(0, 10);
+  lcd.println(currentOctave);
+  lcd.display();
+  Serial.println(currentOctave);
 }
